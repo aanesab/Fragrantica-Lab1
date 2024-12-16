167d1db97ddbaa4b157b0069720b7d9365164dc3
Anesa Bulliqi <anesabulliqi@gmail.com>
2024-12-16 01:37:05 +0100

    Created product model

--- FragranticaSolution/Fragrantica/Models/Category.cs
+++ FragranticaSolution/Fragrantica/Models/Category.cs
@@ -8,8 +8,8 @@ namespace Fragrantica.Models
         public int Id { get; set; }
 
         [Required]
-        [StringLength(50)]
+        
         public string Name { get; set; }
-
+        public int DisplayOrder { get; set; }
     }
 }
