7b1b79e5a5155d81663461ddf7369b9dab14a499
zanasad <150291321+zanasad@users.noreply.github.com>
2024-12-21 00:55:42 +0100

    Category List

--- FragranticaSolution/Fragrantica/obj/Debug/net8.0/Fragrantica.AssemblyInfo.cs
+++ FragranticaSolution/Fragrantica/obj/Debug/net8.0/Fragrantica.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fragrantica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb661713c324c1acaf15f1316aa4741f12559496")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78852f581dab1a28d9794df1268640f9497072ed")]
->>>>>>> f00e41028761bef8b167e111abe924578cd9d95c
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f53655d4bcb414c6353e0b98639dd2ffb3a5b02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fragrantica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fragrantica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
