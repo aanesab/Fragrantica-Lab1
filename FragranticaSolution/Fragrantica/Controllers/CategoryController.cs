3d6ccf59d5545fb4965ad717584071af5aa1f719
Anesa Bulliqi <anesabulliqi@gmail.com>
2025-01-12 05:45:39 +0100

    updates made to categories

--- FragranticaSolution/Fragrantica/Controllers/CategoryController.cs
+++ FragranticaSolution/Fragrantica/Controllers/CategoryController.cs
@@ -23,5 +23,17 @@ namespace Fragrantica.Controllers
 
             return View();
         }
-    }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+}
 }
--- FragranticaSolution/Fragrantica/Models/Category.cs
+++ FragranticaSolution/Fragrantica/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Fragrantica.Models
 {
@@ -6,12 +7,11 @@ namespace Fragrantica.Models
     {
         [Key]
         public int Id { get; set; }
-
         [Required]
-        
-        public required string Name { get; set; }
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 500, ErrorMessage = "Display Order must be between 1-500")]
         public int DisplayOrder { get; set; }
-
-        // test anesa
     }
 }
